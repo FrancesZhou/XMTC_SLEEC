b5f29439bdf86f457667132c5c679cf19658ce66
Frances Zhou <zhouxian@sjtu.edu.cn>
2017-12-10 10:28:11 -0500

    add info

--- make_SLEEC.m
+++ make_SLEEC.m
@@ -2,6 +2,9 @@
 cd SLEEC
 make
 
+cd ../ReadData_Matlab
+make
+
 cd ..
 
 fprintf('SLEEC make complete\n');
--- run_for_wiki10.m
+++ run_for_wiki10.m
@@ -4,22 +4,28 @@ if data_save
     load('dataset/Wiki10/wiki10.mat');
 else
     data = {};
+    fprintf('read train data\n');
     [x, y] = read_data('dataset/Wiki10/wiki10_train.txt');
     data.X = x';
     data.Y = y';
+    fprintf('read test data\n');
     [x, y] = read_data('dataset/Wiki10/wiki10_test.txt');
     data.Xt = x';
     data.Yt = y';
     data.name = 'wiki10';
+    fprintf('save data\n');
     save('dataset/Wiki10/wiki10.mat', 'data');
 end
 % params;
 cd dataset/Wiki10
+fprintf('load params\n');
 wiki10Params
 
 % run SLEEC
 cd ../../../
+fprintf('run SLEEC\n');
 result = SLEEC(data, Params);
+fprintf('save result\n');
 save('ReadData_Matlab/dataset/Wiki10/result.mat', 'result');
 % calculate recall
 
